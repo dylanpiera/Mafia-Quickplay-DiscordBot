9c2050c562b83fc389e72550629aa13cca692452
CheffOfGames <jeffreyhollander1999@gmail.com>
2017-03-21 12:04:07 +0100

    Cookie
    Added the cookie command

---
+++ DiscordBot/Commands/Cookie.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    static class Cookie
+    {
+        public static void createCommand(DiscordClient _client)
+        {
+            _client.GetService<CommandService>().CreateCommand("cookie")
+                .Description("Bot answers with :cookie:")
+                .Do(async e =>
+                {
+                    Message message = await e.Channel.SendMessage(e.User.Mention + " :cookie:");
+                });
+        }
+    }
+}
