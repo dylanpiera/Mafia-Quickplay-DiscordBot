02ba478bf0b34a8f663b0e7658d5053bf2392f5c
SoaringDylan <dylan@fampiera.nl>
2017-06-22 15:40:43 +0200

    Setup Creation PM

--- DiscordBot/Commands/SetupCommand.cs
+++ DiscordBot/Commands/SetupCommand.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
 using Discord.Commands;
+using DiscordBot.Resources;
 using MySql.Data.MySqlClient;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Commands
@@ -13,46 +15,153 @@ namespace DiscordBot.Commands
             _client.GetService<CommandService>().CreateCommand("setup")
                 .Do(async e =>
                 {
+                    if(e.Channel.IsPrivate)
+                    {
+                        await e.Channel.SendMessage("This command can not be started from a PM. Please try to access it through a server. :x:");
+                        return;
+                    }
+
                     MySqlConnection conn = new MySqlConnection(Sneaky.connectionString);
 
                     Message m1 = await e.Channel.SendMessage("Setup Menu\nFor a list of all setups, type `1`\nTo create your own setup, type `2`\nTo edit one of your setups, type `3`");
                     Message m2 = null;
+                    setupState state = setupState.none;
+                    Setup setup = new Setup();
 
                     EventHandler<MessageEventArgs> menuHandler = null, setupCreator = null;
-                    menuHandler = new EventHandler<MessageEventArgs>(async (f, s) => 
+                    menuHandler = new EventHandler<MessageEventArgs>(async (f, s) =>
                     {
                         MessageEventArgs sender = s;
-                        if (sender.User.Id == e.User.Id && e.Channel.Id == sender.Channel.Id)
+                        try
                         {
-                            if (sender.Message.RawText.StartsWith("1"))
+                            if (sender.User.Id == e.User.Id && e.Channel.Id == sender.Channel.Id)
                             {
-                                await sender.Message.Delete();
-                                await m1.Delete();
-                                if (m2 != null) await m2.Delete();
-                                showGameList(e, conn);
-                                _client.MessageReceived -= menuHandler;
-                            }
-                            if (sender.Message.RawText.StartsWith("2"))
-                            {
-                                await sender.Message.Delete();
-                                await m1.Delete();
-                                if (m2 != null) await m2.Delete();
-                                //run create setup handler
+                                if (sender.Message.RawText.StartsWith("1"))
+                                {
 
-                                //add setup creation handler
-                                _client.MessageReceived -= menuHandler;
-                            }
-                            if (sender.Message.RawText.StartsWith("3"))
-                            {
-                                await sender.Message.Delete();
-                                if (m2 != null) await m2.Delete();
-                                m2 = await e.Channel.SendMessage("This feature has yet to be implemented, Returning to menu.");
+                                    await sender.Message.Delete();
+                                    await m1.Delete();
+                                    if (m2 != null) await m2.Delete();
+                                    showGameList(e, conn);
+                                    _client.MessageReceived -= menuHandler;
+                                }
+                                else if (sender.Message.RawText.StartsWith("2"))
+                                {
+
+                                    await sender.Message.Delete();
+                                    if (m2 != null) await m2.Delete();
+                                    //run create setup handler
+                                    await m1.Edit("Welcome to the setup creator. Setups are handled in PMs.\nInitializing setup creator in your pm... :clock2:");
+                                    await e.User.SendMessage("**Welcome to the setup creator.** You can exit at any point by typing `exit`\nPlease provide us with a name for the setup.");
+                                    setup.Creator = s.User;
+                                    state = setupState.name;
+                                    //add setup creation handler
+                                    _client.MessageReceived += setupCreator;
+                                    _client.MessageReceived -= menuHandler;
+                                }
+                                else if (sender.Message.RawText.StartsWith("3"))
+                                {
+
+                                    await sender.Message.Delete();
+                                    if (m2 != null) await m2.Delete();
+                                    m2 = await e.Channel.SendMessage("This feature has yet to be implemented, Returning to menu.");
+                                }
                             }
                         }
+                        catch { }
                     });
-                    setupCreator = new EventHandler<MessageEventArgs>(async (f, s) =>
+                    setupCreator = new EventHandler<MessageEventArgs>(async (f, sender) =>
                     {
-                        await Task.Delay(0);
+                        MessageEventArgs s = sender;
+                        if (s.Message.RawText.ToLower().StartsWith("exit")) { _client.MessageReceived -= setupCreator; await s.User.SendMessage(":x: Setup Aborted :x:"); return; }
+
+                        if (e.User.Id == s.User.Id && e.User.PrivateChannel.Id == s.Channel.Id)
+                            switch (state)
+                            {
+                                case setupState.name:
+                                    setup.SetupName = s.Message.RawText;
+                                    state = setupState.nameReceived;
+                                    await s.Channel.SendMessage($"is: ```{setup.SetupName}``` the name the setup is supposed to have? Yes / No");
+
+                                    break;
+                                case setupState.nameReceived:
+                                    if (s.Message.RawText.ToLower().Equals("y") || s.Message.RawText.ToLower().StartsWith("yes"))
+                                    {
+                                        await s.Channel.SendMessage($"How long will the day phase last (in minutes) for  your game \"{setup.SetupName}\"\n_min 2 min / max 20 min_");
+                                        state = setupState.dayLength;
+                                    }
+                                    else if (s.Message.RawText.ToLower().Equals("n") || s.Message.RawText.ToLower().StartsWith("no"))
+                                    {
+                                        await s.Channel.SendMessage("What is the name of your setup ?");
+                                        state = setupState.name;
+                                    }
+                                    else
+                                    {
+                                        await s.Channel.SendMessage($"Please answer with Yes / No\nIs ```{setup.SetupName}``` the name the setup is supposed to have?");
+                                    }
+                                    break;
+
+                                case setupState.dayLength:
+                                    try
+                                    {
+                                        int condition = Int32.Parse(new string(s.Message.RawText.Where(c => char.IsDigit(c)).ToArray()));
+                                        if (condition >= 2 && condition <= 20)
+                                        {
+                                            setup.DayLength = condition;
+                                            await s.Channel.SendMessage($"How long will the night phase last (in minutes) for  your game \"{setup.SetupName}\"\n_min 2 min / max 20 min_");
+                                            state = setupState.nightLength;
+                                        }
+                                        else await s.Channel.SendMessage("Please provide a number between 2 and 20.");
+                                    }
+                                    catch
+                                    {
+                                        await s.Channel.SendMessage("Please provide a number between 2 and 20.");
+                                    }
+
+                                    break;
+                                case setupState.nightLength:
+                                    try
+                                    {
+                                        int condition = Int32.Parse(new string(s.Message.RawText.Where(c => char.IsDigit(c)).ToArray()));
+                                        if (condition >= 2 && condition <= 20)
+                                        {
+                                            setup.NightLength = condition;
+                                            await s.Channel.SendMessage($"The Day / Night phase lenghts for {setup.SetupName} are: {setup.DayLength} min / {setup.NightLength} min. Is this correct? Yes / No");
+                                            state = setupState.lenghtReceived;
+                                        }
+                                        else await s.Channel.SendMessage("Please provide a number between 2 and 20.");
+                                    }
+                                    catch
+                                    {
+                                        await s.Channel.SendMessage("Please provide a number between 2 and 20.");
+                                    }
+
+                                    break;
+
+                                case setupState.lenghtReceived:
+                                    if (s.Message.RawText.ToLower().Equals("y") || s.Message.RawText.ToLower().StartsWith("yes"))
+                                    {
+                                        await s.Channel.SendMessage($"The setup you created is:\n```Setup Name: {setup.SetupName} | Created by: {setup.CreatorName} | Day/Night time: {setup.DayLength} min / {setup.NightLength} min ```\n:white_check_mark: Step 1 has been completed. The next step is to assign roles.");
+                                        await s.Channel.SendMessage("Roles aren't supported in this version of the setup creator. :x:\n\n:x: Setup couldn't be completed. Aborting setup... :x:");
+                                        _client.MessageReceived -= setupCreator;
+                                    }
+                                    else if (s.Message.RawText.ToLower().Equals("n") || s.Message.RawText.ToLower().StartsWith("no"))
+                                    {
+                                        await s.Channel.SendMessage($"How long will the day phase last (in minutes) for  your game \"{setup.SetupName}\"\n_min 2 min / max 20 min_");
+                                        state = setupState.dayLength;
+                                    }
+                                    else
+                                    {
+                                        await s.Channel.SendMessage($"Please answer with Yes / No");
+                                    }
+
+                                    break;
+
+                                default:
+                                    await Task.Delay(0);
+                                    break;
+                            }
+
                     });
 
                     _client.MessageReceived += menuHandler;
@@ -93,6 +202,7 @@ namespace DiscordBot.Commands
                     Console.WriteLine(exc);
                 }
             }
+
         }
     }
 }
---
+++ DiscordBot/Resources/setupCreatorState.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Resources
+{
+    enum setupState
+    {
+        none,
+        name,
+        nameReceived,
+        lynchTie,
+        majorityType,
+        dayLength,
+        nightLength,
+        lenghtReceived,
+        roles
+    }
+}
